204a26a7a25e5a4beef1c4095d13840924e95e32
Seho-P <54922924+Seho-P@users.noreply.github.com>
2025-11-09 22:40:00 +0900

    [Gold IV] Title: N-Queen, Time: 1648 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -1,55 +1,33 @@
 #include <iostream>
 using namespace std;
 
-int chess[16][16];
-int qcnt;
-int result;
-int qc[16];
-pair<int,int> temc[16];
-int dx[4] = {-1,-1,1,1};
-int dy[4] = {1,-1,-1,1};
+int n;
+int result = 0;
+bool col[16];       // 각 열에 퀸이 있는지
+bool diag1[32];     // ↙↗ 방향 (r+c) , 왼쪽 위부터 계산 
+bool diag2[32];     // ↘↖ 방향 (r-c+n-1),오른쪽 위부터 계산
 
-bool checkqueen(int n,int x,int y){
-    for(int i=0;i<8;i++){
-        temc[i] = {x+dx[i],y+dy[i]};
-    }
-    for(int i=0;i<4;i++){
-        while(temc[i].first>=0 && temc[i].second>=0 && temc[i].first<n &&temc[i].second<n){
-            if(chess[temc[i].first][temc[i].second]==1){
-                return false;
-            }
-            temc[i] = {temc[i].first+dx[i],temc[i].second+dy[i]};
-        }
-    }
-    return true;
-}
-
-
-void dfs(int n,int rank){
-    if(rank==n){
+void dfs(int row) {
+    if (row == n) {
         result++;
         return;
     }
-    for(int i=0;i<n;i++){
-        if(qc[i]!=1 &&checkqueen(n,rank,i)){
-            chess[rank][i]=1;
-            qc[i]=1;
-            qcnt++;
-            dfs(n,rank+1);
-            chess[rank][i]=0;
-            qcnt--;
-            qc[i]=0;
-        }
-        
+
+    for (int c = 0; c < n; c++) {
+        if (col[c] || diag1[row + c] || diag2[row - c + n - 1])
+            continue;  // 공격 가능 위치면 skip
+
+        col[c] = diag1[row + c] = diag2[row - c + n - 1] = true;
+        dfs(row + 1);
+        col[c] = diag1[row + c] = diag2[row - c + n - 1] = false;
     }
-    return;
 }
 
-int main(){
+int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int n;
+
     cin >> n;
-    dfs(n,0);
+    dfs(0);
     cout << result;
 }
