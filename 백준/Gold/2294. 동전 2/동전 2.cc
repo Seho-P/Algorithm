63cd0dbd31715a9cfb89ff2612a99a42e1f734de
Seho-P <54922924+Seho-P@users.noreply.github.com>
2025-11-24 21:38:27 +0900

    [Gold V] Title: 동전 2, Time: 0 ms, Memory: 2064 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.cc
+++ 백준/Gold/2294. 동전 2/동전 2.cc
@@ -14,24 +14,12 @@ int main(){
         coin.push_back(tem);
     }
     sort(coin.begin(),coin.end());
-    for(int i=0;i<=k;i++){
+    for(int i=1;i<=k;i++){
         acost[i] = 999999;
     }
     for(int i=0;i<n;i++){
-        if(coin[i]<=10000){
-            acost[coin[i]]=1;
-        }
-        
-    }
-    for(int i=1;i<=k;i++){
-        tem = 999999;
-        for(int j=0;j<n;j++){
-            if(i>coin[j] && acost[i-coin[j]]!=0){
-                tem = min(tem,acost[i-coin[j]]+1);
-            }
-        }
-        if(tem!=999999){
-            acost[i]=min(acost[i],tem);
+        for(int j=coin[i];j<=k;j++){
+            acost[j] = min(acost[j],acost[j-coin[i]]+1);
         }
         
     }
