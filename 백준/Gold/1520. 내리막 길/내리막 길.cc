605017ff0fbf3ffce7b502737db4580c1d013755
Seho-P <54922924+Seho-P@users.noreply.github.com>
2025-11-24 23:48:20 +0900

    [Gold III] Title: 내리막 길, Time: 92 ms, Memory: 4532 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.cc
+++ 백준/Gold/1520. 내리막 길/내리막 길.cc
@@ -3,11 +3,12 @@ using namespace std;
 
 int doro[501][501];
 int dp[501][501];
+int m,n;
 
 int dx[4] = {1,0,0,-1}; 
 int dy[4] = {0,1,-1,0};
 
-int dfs(int y,int x,int m,int n){
+int dfs(int y,int x){
     if(y==m-1 && x==n-1)return 1;
     if(dp[y][x]!=-1) return dp[y][x];
     dp[y][x]=0;
@@ -16,14 +17,13 @@ int dfs(int y,int x,int m,int n){
         int yy = y+dy[i];
         if(xx<0 || xx>=n || yy<0 || yy>=m)continue;
         if(doro[y][x]>doro[yy][xx]){
-                dp[y][x] = dp[y][x] + dfs(yy,xx,m,n);
+                dp[y][x] = dp[y][x] + dfs(yy,xx);
         }
     }
     return dp[y][x];
 }
 
 int main(){
-    int m,n;
     cin >> m >> n;
     for(int i=0;i<m;i++){
         for(int j=0;j<n;j++){
@@ -31,7 +31,7 @@ int main(){
             dp[i][j]=-1;
         }
     }
-    dfs(0,0,m,n);
+    dfs(0,0);
     cout << dp[0][0];
 }
 
