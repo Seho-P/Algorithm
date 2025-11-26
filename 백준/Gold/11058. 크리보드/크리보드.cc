f513b3ba6cd5c7ccbb0a813bd56e6e4c62e25c99
Seho-P <54922924+Seho-P@users.noreply.github.com>
2025-11-26 23:41:22 +0900

    [Gold V] Title: 크리보드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/11058. 크리보드/크리보드.cc
+++ 백준/Gold/11058. 크리보드/크리보드.cc
@@ -6,13 +6,13 @@ long long dp[101];
 int main(){
     int n;
     cin >> n;
-    for(int i=0;i<=n;i++){
-        dp[i]=i;
-    }
-    for(int i=4;i<=n;i++){
-        for(int j=2;j<i-1;j++){
-            dp[i] = max(dp[i],dp[i-j-1]*j);
+
+    for(int i = 1; i <= n; i++){
+        dp[i] = i;
+        for(int k = 1; k <= i - 3; k++){
+            dp[i] = max(dp[i], dp[k] * (i - k - 1));
         }
     }
+
     cout << dp[n];
 }
