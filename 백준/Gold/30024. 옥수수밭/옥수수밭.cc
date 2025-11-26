fe3cf133fe411abc813fa5b910c8b7e78ce4a37f
Seho-P <54922924+Seho-P@users.noreply.github.com>
2025-11-26 23:32:42 +0900

    [Gold IV] Title: 옥수수밭, Time: 132 ms, Memory: 14584 KB -BaekjoonHub

--- 백준/Gold/30024. 옥수수밭/옥수수밭.cc
+++ 백준/Gold/30024. 옥수수밭/옥수수밭.cc
@@ -1,46 +1,55 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 #include <queue>
 using namespace std;
 
 int corn[1001][1001];
 int visited[1001][1001];
-pair<int,int> cornidx[1000001];
 
 int dx[4] = {0,0,1,-1};
 int dy[4] = {1,-1,0,0};
 
 int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
     int n,m,k;
     cin >> n >> m;
-    priority_queue<int> poscorn;
+
+    priority_queue<pair<int, pair<int,int>>> poscorn;
+
     for(int i=1;i<=n;i++){
         for(int j=1;j<=m;j++){
             cin >> corn[i][j];
             if(i==1 || j==1 || i==n || j==m){
-                visited[i][j]=1;
-                poscorn.push(corn[i][j]);
+                // 초기에는 visited 표시하지 않음
+                poscorn.emplace(corn[i][j], make_pair(i,j));
             }
-            cornidx[corn[i][j]]={i,j};
-            
         }
     }
     cin >> k;
-    visited[cornidx[poscorn.top()].first][cornidx[poscorn.top()].second] = 1;
-    while(k--){
-        auto l = poscorn.top();
+
+    int printed = 0;
+    while(printed < k && !poscorn.empty()){
+        auto cur = poscorn.top();
         poscorn.pop();
-        cout << cornidx[l].first <<" " << cornidx[l].second << "\n";
+        int x = cur.second.first;
+        int y = cur.second.second;
+
+        if(visited[x][y]) continue; // 이 경우 printed를 증가시키지 않음
+
+        visited[x][y] = 1;
+        cout << x << " " << y << '\n';
+        printed++;
+
         for(int i=0;i<4;i++){
-            int xx = cornidx[l].first + dx[i];
-            int yy = cornidx[l].second + dy[i];
-            if(xx<1 || xx>n || yy<1 || yy>m)continue;
+            int xx = x + dx[i];
+            int yy = y + dy[i];
+            if(xx<1 || xx>n || yy<1 || yy>m) continue;
             if(!visited[xx][yy]){
-                visited[xx][yy] = 1;
-                poscorn.push(corn[xx][yy]);
+                poscorn.emplace(corn[xx][yy], make_pair(xx,yy));
             }
-            
         }
     }
+    return 0;
 }
