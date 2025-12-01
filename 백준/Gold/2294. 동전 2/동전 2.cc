f4199c8ddf3835a5fee0168d884a47c88a90245e
Seho-P <54922924+Seho-P@users.noreply.github.com>
2025-12-01 19:59:09 +0900

    [Gold V] Title: 동전 2, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.cc
+++ 백준/Gold/2294. 동전 2/동전 2.cc
@@ -1,34 +1,32 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 using namespace std;
 
 int acost[10001];
+int coin[101];
 
 int main(){
-    int n,k,tem;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, k;
     cin >> n >> k;
-    vector<int> coin;
-    for(int i=0;i<n;i++){
-        cin >> tem;
-        coin.push_back(tem);
-    }
-    sort(coin.begin(),coin.end());
-    for(int i=1;i<=k;i++){
-        acost[i] = 999999;
-    }
-    for(int i=0;i<n;i++){
-        for(int j=coin[i];j<=k;j++){
-            acost[j] = min(acost[j],acost[j-coin[i]]+1);
+
+    for(int i = 0; i < n; i++)
+        cin >> coin[i];
+
+    const int INF = 1e9;
+
+    for(int i = 1; i <= k; i++)
+        acost[i] = INF;
+
+    acost[0] = 0;
+
+    for(int i = 0; i < n; i++){
+        for(int j = coin[i]; j <= k; j++){
+            if(acost[j - coin[i]] != INF)
+                acost[j] = min(acost[j], acost[j - coin[i]] + 1);
         }
-        
     }
-    if(acost[k]!=999999){
-        cout << acost[k];
-    }
-    else{
-        cout << -1;
-    }
-    
-    
+
+    cout << (acost[k] == INF ? -1 : acost[k]);
 }
