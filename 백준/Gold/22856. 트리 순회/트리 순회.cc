a58964e009a8276e5d4e73d9535c3f81287338ea
Seho-P <54922924+Seho-P@users.noreply.github.com>
2025-12-04 20:15:12 +0900

    [Gold IV] Title: 트리 순회, Time: 28 ms, Memory: 4960 KB -BaekjoonHub

--- 백준/Gold/22856. 트리 순회/트리 순회.cc
+++ 백준/Gold/22856. 트리 순회/트리 순회.cc
@@ -3,48 +3,53 @@
 using namespace std;
 
 int n;
-int result;
-int alltree=0;
+int result = 0;
+int visitedCount = 0;
 int visited[100001];
-vector<pair<int,int>> stree(100001);
+vector<pair<int,int>> tree(100001);
 
-void dfs(int x){
-    // cout << alltree << " " << x << endl ;
-    if(visited[x]==1){
-        if(alltree==n){
+void dfs(int x) {
+    int L = tree[x].first;
+    int R = tree[x].second;
+
+    // 왼쪽 자식
+    if (L != -1 && visited[L] == 0) {
+        result++;           // L로 이동
+        dfs(L);
+        result++;           // 다시 x로 복귀
+    }
+
+    // 자기 자신 방문
+    if (!visited[x]) {
+        visited[x] = 1;
+        visitedCount++;
+
+        // 모든 노드 방문 완료 → 출력 후 종료
+        if (visitedCount == n) {
             cout << result;
             return;
         }
     }
-    int nowl = stree[x].first;
-    int nowr = stree[x].second;
-    if(nowl!= -1 && visited[nowl]==0){
+
+    // 오른쪽 자식
+    if (R != -1 && visited[R] == 0) {
         result++;
-        dfs(nowl);
+        dfs(R);
         result++;
-    }
-    if(visited[x]==0){
-        visited[x]=1;
-        alltree++;
-        if(alltree==n){
-            cout << result;
-            return;
-        }
-    }
-    if(nowr!=-1 && visited[nowr]==0){
-        result++;
-        dfs(nowr);
-        result++;
+        // 여기서는 복귀를 증가시키지 않음 (마지막 노드면 복귀하지 않기 때문)
     }
 }
 
-int main(){
-    int a,b,c;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     cin >> n;
-    
-    for(int i=0;i<n;i++){
+    for (int i = 0, a, b, c; i < n; i++) {
         cin >> a >> b >> c;
-        stree[a] = {b,c}; 
+        tree[a] = {b, c};
     }
+
     dfs(1);
+    return 0;
 }
