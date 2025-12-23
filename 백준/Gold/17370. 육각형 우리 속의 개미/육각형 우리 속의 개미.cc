d242fa1dd7ee24dec17cbd0acc85ea00b9ae5485
Seho-P <54922924+Seho-P@users.noreply.github.com>
2025-12-23 23:55:20 +0900

    [Gold III] Title: 육각형 우리 속의 개미, Time: 12 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/17370. 육각형 우리 속의 개미/육각형 우리 속의 개미.cc
+++ 백준/Gold/17370. 육각형 우리 속의 개미/육각형 우리 속의 개미.cc
@@ -5,26 +5,24 @@ using namespace std;
 int N;
 int answer;
 int visited[101][101];
+// 0: 아래
+// 1: 오른쪽 아래
+// 2: 오른쪽 위
+// 3: 위
+// 4: 왼쪽 위
+// 5: 왼쪽 아래
 int dy[] = {1,1,-1,-1,-1,1};
 int dx[] = {0,1,1,0,-1,-1};
 
-//oxo
-//xox
-//xox
-//oxoxC
-//oxox
-//xoxo
 
-// 이러면 right +1계속 해주면 잘 돌아감
-// left는 +5 ㅇㅋ 이거다 
 
 void dfs(int y,int x,int dir,int move){
-    if(move==0){
-        if(visited[y][x]==0)return;
-        answer++;
-        return;
+    if (visited[y][x]) {
+        if (move == 0) answer++;
+            return;
     }
-    else if(visited[y][x]==1)return;
+    if (move == 0) return;
+
     
     int right_dir = (dir+1)%6;
     int left_dir  = (dir+5)%6;
